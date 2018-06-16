8f5ed5c30b0fbfe9a41b5660e6b43a8ebc2cc701
coldBl00d <akhilrajr28@gmail.com>
2018-06-16 01:52:47 -0700

    Added test for the generated query

--- src/com/ds/analyser/Analyser.java
+++ src/com/ds/analyser/Analyser.java
@@ -7,7 +7,7 @@ import com.ds.enities.Table;
 
 public class Analyser {
 	
-	private final String CHILD_COUNT="SELECT count(1), :fktoparent: FROM :childTable group by :fktoparent:";
+	private final String CHILD_COUNT="SELECT count(1), :fktoparent: FROM :childTable: group by :fktoparent:";
 	
 	public void process(ArrayList<Shape> shapes) {
 		for(Shape cShape : shapes) {
@@ -16,13 +16,20 @@ public class Analyser {
 	}
 	
 	public void process(Shape shape) {
+		
 		Table fromTable = shape.getFromTable();
 		Table toTable = shape.getToTable();
 		assert fromTable!=null && toTable!=null;
+		String query = makeQuery(fromTable, toTable);
+	}
+	
+	public String makeQuery(Table fromTable, Table toTable) {
 		String query = new String(CHILD_COUNT); 
-		query.replaceAll(":fktoparent:", fromTable.getLink().getLinkName());
-		query.replaceAll(":childTable:", fromTable.getTable());
+		query=query.replace(":fktoparent:", fromTable.getLink().getLinkName());
 		System.out.println(query);
+		query=query.replace(":childTable:", fromTable.getTable());
+		System.out.println(query);
+		return query;
 	}
 	
 	
---
+++ src/com/ds/tests/AnalyzerTest.java
@@ -0,0 +1,5 @@
+package com.ds.tests;
+
+public class AnalyzerTest {
+
+}
