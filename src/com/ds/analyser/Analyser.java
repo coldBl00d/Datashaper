bf85748d57684e6629133e6a2cf8e413fcf2640a
coldBl00d <akhilrajr28@gmail.com>
2018-06-16 00:58:22 -0700

    Analyser now generates the query to fire to the database

--- src/com/ds/analyser/Analyser.java
+++ src/com/ds/analyser/Analyser.java
@@ -1,5 +1,29 @@
 package com.ds.analyser;
 
-public class Analyser {
+import java.util.ArrayList;
 
+import com.ds.enities.Shape;
+import com.ds.enities.Table;
+
+public class Analyser {
+	
+	private final String CHILD_COUNT="SELECT count(1), :fktoparent: FROM :childTable group by :fktoparent:";
+	
+	public void process(ArrayList<Shape> shapes) {
+		for(Shape cShape : shapes) {
+			process(cShape);
+		}
+	}
+	
+	public void process(Shape shape) {
+		Table fromTable = shape.getFromTable();
+		Table toTable = shape.getToTable();
+		assert fromTable!=null && toTable!=null;
+		String query = new String(CHILD_COUNT); 
+		query.replaceAll(":fktoparent:", fromTable.getLink().getLinkName());
+		query.replaceAll(":childTable:", fromTable.getTable());
+		System.out.println(query);
+	}
+	
+	
 }
